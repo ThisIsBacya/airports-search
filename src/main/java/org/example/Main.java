192aa4b75b9df93f2e2c3c106f2ad4fa2ec404ed
Bacya <bacyadaniil@gmail.com>
2023-02-14 20:22:49 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static List<String> searchCsvLine(String[] searchColumnIndex, String searchString) throws IOException {
+        String regex = searchString + "[a-zA-Z. ]+";
+        Pattern pattern = Pattern.compile(regex);
+        BufferedReader br = new BufferedReader(new FileReader("E://IdeaProjects//airports-search//airports.csv"));
+        String line;
+        List<String> rows = new ArrayList<>();
+        long time = System.currentTimeMillis();
+        while ( (line = br.readLine()) != null ) {
+            String[] values = line.split(",");
+            Matcher matcher = pattern.matcher(values[Integer.parseInt(searchColumnIndex[1])]);
+            if (matcher.find()) {
+                rows.add(values[Integer.parseInt(searchColumnIndex[1].substring(matcher.start(), matcher.end()))]);
+            }
+        }
+        System.out.println("Количество найденных строк: " + rows.size());
+        System.out.println("Времени затрачено: " + (System.currentTimeMillis() - time) + "ms");
+        br.close();
+        return rows;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введите строку: ");
+            String text = scanner.nextLine();
+            System.out.println(searchCsvLine(args, text));
+            if (text.equals("!quit")) {
+                System.exit(0);
+            }
+        } while (true);
+    }
+}
